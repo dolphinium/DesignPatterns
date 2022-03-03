37b394b7f99e94f394d24fa434fef126eb44aede
dolphinium <dolphofficial@gmail.com>
2022-03-04 00:50:49 +0300

    Facade Design.

---
+++ Facade/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Save();
+            Console.ReadLine();
+        }
+    }
+
+    class Logging : ILogging
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged!");
+        }
+    }
+
+    internal interface ILogging
+    {
+        void Log();
+    }
+
+    class Caching : ICaching
+    {
+        public void Cache()
+        {
+            Console.WriteLine("Cached!");
+        }
+    }
+
+    internal interface ICaching
+    {
+        void Cache();
+    }
+
+    class Authorize : IAuthorize
+    {
+        public void CheckUser()
+        {
+            Console.WriteLine("User checked!");
+        }
+    }
+
+
+    internal interface IAuthorize
+    {
+        void CheckUser();
+    }
+
+    class Validation : IValidate
+    {
+        public void Validate()
+        {
+            Console.WriteLine("Validated!");
+        }
+    }
+
+
+    internal interface IValidate
+    {
+        void Validate();
+    }
+
+    class CustomerManager
+    {
+        private CrossCuttingConcernsFacade _concerns;
+
+        public CustomerManager()
+        {
+            _concerns = new CrossCuttingConcernsFacade();
+        }
+
+        public void Save()
+        {
+            _concerns.Caching.Cache();
+            _concerns.Logging.Log();
+            _concerns.Authorize.CheckUser();
+            _concerns.Validate.Validate();
+            Console.WriteLine("Saved to db");
+        }
+    }
+
+    class CrossCuttingConcernsFacade
+    {
+        public ILogging Logging;
+        public ICaching Caching;
+        public IAuthorize Authorize;
+        public IValidate Validate;
+
+        public CrossCuttingConcernsFacade()
+        {
+            Logging = new Logging();
+            Caching = new Caching();
+            Authorize = new Authorize();
+            Validate = new Validation();
+        }
+    }
+}
