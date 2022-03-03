5f184f563ead22ca0cd39506d08f84624701c632
dolphinium <dolphofficial@gmail.com>
2022-03-03 19:19:11 +0300

    Singleton design pattern.

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -10,6 +10,29 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
+            var customerManager = CustomerManager.CreateAsSingleton();      // we can not create a customer manager object via CustomerManager customerManager = new CustomerManager(); 
+            customerManager.Save();
+
+            Console.ReadLine();
+        }
+    }
+
+    class CustomerManager
+    {
+        private static CustomerManager _customerManager; // create static field for object
+
+        private CustomerManager() // initialize a private constructor
+        {
+        }
+
+        public static CustomerManager CreateAsSingleton()
+        {
+            return _customerManager ?? (_customerManager = new CustomerManager());      // if customer manager instance didn't creating yet, create a instance
+        }
+
+        public void Save()       // if we use static we can not reach the save operation via instance of customer manager!
+        {
+            Console.WriteLine("Saved!");
         }
     }
 }
