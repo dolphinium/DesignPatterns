bf0e6e5a633a20e96044f2f38e8b4a3fe83e5e85
dolphinium <dolphofficial@gmail.com>
2022-03-04 21:41:03 +0300

    Dependency Injection. (Via Ninject)

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+
+namespace DependencyInjection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IKernel kernel = new StandardKernel();          // IoC Container 
+            kernel.Bind<IProductDal>().To<NhProductDal>().InSingletonScope();       // Creating instance in Singleton scope
+
+            ProductManager productManager = new ProductManager(kernel.Get<IProductDal>());
+            productManager.Save();
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IProductDal
+    {
+        void Save();
+    }
+
+    class EfProductDal:IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with Ef");
+        }
+    }
+
+
+    class NhProductDal : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with Nh");
+        }
+    }
+
+
+
+    class ProductManager
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Save()
+        {
+            //  Business codes
+            
+            _productDal.Save();
+        }
+    }
+}
